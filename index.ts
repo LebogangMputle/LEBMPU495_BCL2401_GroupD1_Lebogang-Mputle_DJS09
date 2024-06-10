2fb7340bc78f10fd28bb342424a370fa3e107eff
LebogangMputle <lebozikalala@gmail.com>
2024-06-10 16:20:18 +0200

    "Fix typo in variable declaration: 'booleanS' -> 'boolean'"

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ const returningUserDisplay = document.querySelector('#returning-user')
 const userNameDisplay = document.querySelector('#user')
 const reviewTotalDisplay = document.querySelector('#reviews')
 
-let isOpen : booleanS
+let isOpen : boolean
 
 const reviews = [
     {
